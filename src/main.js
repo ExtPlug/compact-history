1619e1bfda1aef36666efa015c601e569cbea04c
René Kooi <rene@kooi.me>
2015-08-04 17:06:18 +0200

    use declarative style:{} instead of deprecated Style()

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 define(function (require, exports, module) {
 
   const Plugin = require('extplug/Plugin');
-  const _ = require('underscore');
-  const $ = require('jquery');
 
   const ITEM_HEIGHT = 20;
 
@@ -10,57 +8,53 @@ define(function (require, exports, module) {
     name: 'Compact History',
     description: 'Lays out the room history in a much more compact view.',
 
-    // We'll just use CSS
-    enable() {
-      this._super();
-      this.Style({
-        '#history-panel .media-list.history .playlist-media-item:not(.selected)': {
+    style: {
+      '#history-panel .media-list.history .playlist-media-item:not(.selected)': {
+        'height': `${ITEM_HEIGHT}px`,
+
+        'img': {
           'height': `${ITEM_HEIGHT}px`,
+          'width': `${ITEM_HEIGHT * 1.5}px`,
+          'margin-top': '0px'
+        },
 
-          'img': {
+        '.score': {
+          'height': 'auto',
+          'width': 'auto',
+          'top': '0px',
+          'left': '65px',
+          '.item': {
+            'margin-right': '10px'
+          }
+        },
+
+        '.meta': {
+          'height': 'auto',
+          'span': {
             'height': `${ITEM_HEIGHT}px`,
-            'width': `${ITEM_HEIGHT * 1.5}px`,
-            'margin-top': '0px'
+            'top': '0px'
           },
-
-          '.score': {
-            'height': 'auto',
-            'width': 'auto',
-            'top': '0px',
-            'left': '65px',
-            '.item': {
-              'margin-right': '10px'
-            }
+          '.author': {
+            'left': '120px',
+            'right': '300px',
+            'width': 'auto'
           },
+          '.name': {
+            'right': '125px'
+          }
+        },
 
-          '.meta': {
-            'height': 'auto',
-            'span': {
-              'height': `${ITEM_HEIGHT}px`,
-              'top': '0px'
-            },
-            '.author': {
-              'left': '120px',
-              'right': '300px',
-              'width': 'auto'
-            },
-            '.name': {
-              'right': '125px'
-            }
-          },
-
-          '.actions': {
+        '.actions': {
+          'height': `${ITEM_HEIGHT}px`,
+          'top': '0px',
+          'div': {
             'height': `${ITEM_HEIGHT}px`,
-            'top': '0px',
-            'div': {
-              'height': `${ITEM_HEIGHT}px`,
-              'i': {
-                'top': '-4px'
-              }
+            'i': {
+              'top': '-4px'
             }
           }
         }
-      });
+      }
     }
 
   });
